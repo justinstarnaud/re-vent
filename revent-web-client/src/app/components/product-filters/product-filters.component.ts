fe6050a745156f183ae929729103c025dba0ee21
Achille Saint-Hillier <achillesainthillier@gmail.com>
2023-07-21 17:30:10 -0400

    checkbox logic and style

--- revent-web-client/src/app/components/product-filters/product-filters.component.ts
+++ revent-web-client/src/app/components/product-filters/product-filters.component.ts
@@ -7,6 +7,8 @@ import {
   OnChanges,
   SimpleChanges,
   ChangeDetectorRef,
+  ViewChild,
+  ElementRef,
 } from '@angular/core';
 import { filters } from 'src/app/interfaces/filters';
 import { Product } from '../../../../../common/product';
@@ -21,9 +23,13 @@ export class ProductFiltersComponent implements OnInit, OnChanges {
   @Output() updateFilters = new EventEmitter<Map<string, any[]>>();
   @Input() category: string;
   @Input() currentProducts: Product[];
+  @ViewChild('form') form!: ElementRef<HTMLFormElement>;
   min: number;
   max: number;
   productFilters: Map<string, any[]>;
+  selectedFilters: Map<string, any[]>;
+
+  keys: string[];
   constructor(private ref: ChangeDetectorRef) {
     this.category = '';
     this.min = NaN;
@@ -34,14 +40,14 @@ export class ProductFiltersComponent implements OnInit, OnChanges {
     this.productFilters = new Map<string, any[]>([
       ['price', [this.min, this.max]],
     ]);
+    this.selectedFilters = new Map<string, any[]>([]);
     this.currentProducts = [];
+    this.keys = ['price'];
   }
 
   update() {
     this.updateFilters.emit(this.productFilters);
   }
-
-  // TODO: Parmis tous les items disponibles après filtrage par options choisies, afficher seulement les filtres possibles. (CHECK SUR NIKE)
 
   // slider
 
@@ -58,11 +64,6 @@ export class ProductFiltersComponent implements OnInit, OnChanges {
     this.currentProducts.forEach((p: Product) => {
       if (p.filters) {
         Object.keys(p.filters).forEach((v: string) => {
-          // if (this.productFilters[v as keyof filters]) this.productFilters[v as keyof filters]!.push(p.filters[v as keyof ClothingFilter]);
-          // else this.productFilters[v as keyof filters] = [p.filters[v as keyof ClothingFilter]];
-          // if (this.productFilters[v as keyof filters]) this.productFilters[v as keyof filters].push('23');
-          // else this.productFilters[v as keyof filters] = ['to'];
-
           if (this.productFilters.get(v)) {
             const existingFilters = this.productFilters.get(v)!;
             const newFilter = p.filters![v as keyof ClothingFilter];
@@ -73,6 +74,8 @@ export class ProductFiltersComponent implements OnInit, OnChanges {
         });
       }
     });
+
+    this.keys = Array.from(this.productFilters.keys());
   }
 
   isNan(int: number) {
@@ -135,5 +138,30 @@ export class ProductFiltersComponent implements OnInit, OnChanges {
       parseInt(sliderOne.value),
       parseInt(sliderTwo.value),
     ]);
+  }
+
+  toggleFilter(category: string, option: string, event: Event) {
+    const checked = (
+      (event.currentTarget as HTMLElement)!.children[0] as HTMLInputElement
+    ).checked;
+    let currentFilter = this.selectedFilters.get(category);
+    if (currentFilter) {
+      if (checked) {
+        currentFilter.push(option);
+        this.selectedFilters.set(category, [...new Set(currentFilter)]);
+      } else {
+        currentFilter = currentFilter.filter((op: string) => {
+          return op !== option;
+        });
+        this.selectedFilters.set(category, [...new Set(currentFilter)]);
+        if (currentFilter.length === 0) {
+          this.selectedFilters.delete(category);
+        }
+      }
+    } else if (checked) {
+      this.selectedFilters.set(category, [option]);
+    }
+
+    console.log(this.selectedFilters);
   }
 }
